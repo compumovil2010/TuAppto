f9eee8822c49911ee0f7d8ebade066b586251288
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-03-01 20:35:30 -0500

    Create the principal layout

---
+++ app/src/main/java/com/example/tuappto/BuyerMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuappto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyerMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_menu);
+    }
+}
