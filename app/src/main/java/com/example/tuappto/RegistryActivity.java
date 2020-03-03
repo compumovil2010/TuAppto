9df81b4adbb463c7b5dd18e960f4e4fc55bac172
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-03-03 01:43:22 -0500

    Se adelanto un poco el registro

--- app/src/main/java/com/example/tuappto/RegistryActivity.java
+++ app/src/main/java/com/example/tuappto/RegistryActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class InterestedActivity extends AppCompatActivity {
+public class RegistryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interested);
+        setContentView(R.layout.activity_registry);
     }
 }
