41b99aaea3206c8f37a3c0eb17c28a3c28de83db
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-03-03 00:34:33 -0500

    Se creo el item de los interesados

---
+++ app/src/main/java/com/example/tuappto/InterestedActivity.java
@@ -0,0 +1,14 @@
+dpackage com.example.tuappto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterestedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interested);
+    }
+}
