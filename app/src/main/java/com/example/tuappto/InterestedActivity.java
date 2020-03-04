9d4f65e92b56bbbef0e7f56fba930487fc3b0b85
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-03-04 00:30:02 -0500

    Se creo la vista de los chats

--- app/src/main/java/com/example/tuappto/InterestedActivity.java
+++ app/src/main/java/com/example/tuappto/InterestedActivity.java
@@ -2,13 +2,27 @@ package com.example.tuappto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InterestedActivity extends AppCompatActivity {
+
+    Button buttonChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interested);
+
+        buttonChat = findViewById(R.id.button4);
+
+        buttonChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),ChatActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tuappto/RegistryActivity.java
+++ app/src/main/java/com/example/tuappto/RegistryActivity.java
@@ -2,13 +2,27 @@ package com.example.tuappto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistryActivity extends AppCompatActivity {
+
+    Button buttonContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry);
+
+        buttonContinue = findViewById(R.id.buttonContinue);
+
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),ChooseActivity.class));
+            }
+        });
     }
 }
