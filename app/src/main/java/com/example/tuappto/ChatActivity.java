cff5651afcaa2d86ae5fc8d8db3750bcda2d2aba
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-03-04 01:35:06 -0500

    se implemento para poder agendar citas

--- app/src/main/java/com/example/tuappto/ChatActivity.java
+++ app/src/main/java/com/example/tuappto/ChatActivity.java
@@ -2,13 +2,27 @@ package com.example.tuappto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChatActivity extends AppCompatActivity {
+
+    Button buttonDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        buttonDate = findViewById(R.id.button2);
+
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),MakeDateActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tuappto/MakeDateActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuappto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_date);
+    }
+}
