253b57777fef61d59a0668c956e568ababe9a29b
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-04-06 21:36:07 -0500

    Se agregaron los permisos de acceso a la camara y galeria, con su respectivo uso

--- app/src/main/java/com/example/tuappto/PublishPropertyActivity.java
+++ app/src/main/java/com/example/tuappto/PublishPropertyActivity.java
@@ -1,14 +1,154 @@
 package com.example.tuappto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class PublishPropertyActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CAMERA_ID = 1;
+    private static final int PERMISSION_GALLERY_ID = 2;
+    private static final int REQUEST_IMAGE_CAPTURE = 3;
+    private static final int IMAGE_PICKER_REQUEST = 4;
+
+    ImageButton imageButtonCamera;
+    ImageButton imageButtonGallery;
+    ImageView imageViewUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_property);
+
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGallery = findViewById(R.id.imageButtonGallery);
+        imageViewUser = findViewById(R.id.imageViewUser);
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto();
+                }
+                else {
+                    requestPermission(PublishPropertyActivity.this, Manifest.permission.CAMERA, "Acceso a cámara necesario", PERMISSION_CAMERA_ID);
+                }
+            }
+        });
+
+        imageButtonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Intent pickImage = new Intent(Intent.ACTION_PICK);
+                    pickImage.setType("image/*");
+                    startActivityForResult(pickImage, IMAGE_PICKER_REQUEST);
+
+                } else {
+                    requestPermission(PublishPropertyActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE, "Leer almacenamiento es necesario", PERMISSION_GALLERY_ID);
+                }
+            }
+        });
     }
+
+    private void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
+
+        if (ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
+                Toast.makeText(context, justificacion, Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permiso}, idCode);
+        }
+    }
+
+    private void takePhoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_IMAGE_CAPTURE:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bundle extras = data.getExtras();
+                        Bitmap imageBitmap;
+                        if (extras != null) {
+                            imageBitmap = (Bitmap) extras.get("data");
+                            imageViewUser.setImageBitmap(imageBitmap);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+
+            case IMAGE_PICKER_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream;
+
+                        if (imageUri != null) {
+                            imageStream = getContentResolver().openInputStream(imageUri);
+                            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                            imageViewUser.setImageBitmap(selectedImage);
+                        }
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CAMERA_ID: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto();
+
+                } else{
+                    Toast.makeText(this, "Permiso necesario para acceder a la camara", Toast.LENGTH_LONG).show();
+                }
+
+            }
+            break;
+
+            case PERMISSION_GALLERY_ID: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent pickImage = new Intent(Intent.ACTION_PICK);
+                    pickImage.setType("image/*");
+                    startActivityForResult(pickImage, IMAGE_PICKER_REQUEST);
+
+                } else{
+                    Toast.makeText(this, "Permiso necesario para acceder a la galeria", Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+        }
+    }
+
 }
