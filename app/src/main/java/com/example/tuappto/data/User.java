aafec370849a8580fd4e10247a776b0828dab79f
Juandoc17 <juan.doc17@hotmail.com>
2020-04-17 12:23:32 -0500

    Creacion usuario Database

--- app/src/main/java/com/example/tuappto/data/User.java
+++ app/src/main/java/com/example/tuappto/data/User.java
@@ -1,4 +1,50 @@
 package com.example.tuappto.data;
 
 public class User {
+    String nombre;
+    String apellido;
+    String Email;
+    String telefono;
+
+    public User() {
+    }
+
+    public User(String nombre, String apellido, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        Email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
