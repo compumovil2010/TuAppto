4a0be5beb0413dd16e1a2a69af5fa43410e0fecd
Pablo Veintemilla <pbr.9816@gmail.com>
2020-04-21 20:31:07 -0500

    mapas en propiedad

--- app/src/main/java/com/example/tuappto/PublishPropertyActivity.java
+++ app/src/main/java/com/example/tuappto/PublishPropertyActivity.java
@@ -1,9 +1,9 @@
 package com.example.tuappto;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.Activity;
@@ -11,27 +11,62 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class PublishPropertyActivity extends AppCompatActivity {
+public class PublishPropertyActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int PERMISSION_CAMERA_ID = 1;
     private static final int PERMISSION_GALLERY_ID = 2;
     private static final int REQUEST_IMAGE_CAPTURE = 3;
     private static final int IMAGE_PICKER_REQUEST = 4;
-
     ImageButton imageButtonCamera;
     ImageButton imageButtonGallery;
     ImageView imageViewUser;
+
+    private GoogleMap mMap;
+
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    Location destiny;
+    Geocoder mGeocoder;
+    TextView mAddress;
+
+    private static final int REQUEST_CODE = 101;
+    public static final double lowerLeftLatitude = 1.396967;
+    public static final double lowerLeftLongitude= -78.903968;
+    public static final double upperRightLatitude= 11.983639;
+    public static final double upperRigthLongitude= -71.869905;
+    private static final double RADIUS_OF_EARTH_KM = 6371;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,8 +102,14 @@ public class PublishPropertyActivity extends AppCompatActivity {
                 }
             }
         });
-    }
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLocation();
+        destiny = new Location("");
+        mGeocoder = new Geocoder(getBaseContext());
+
+
+    }
     private void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
 
         if (ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED) {
@@ -78,14 +119,12 @@ public class PublishPropertyActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(context, new String[]{permiso}, idCode);
         }
     }
-
     private void takePhoto() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
@@ -133,7 +172,11 @@ public class PublishPropertyActivity extends AppCompatActivity {
                 } else{
                     Toast.makeText(this, "Permiso necesario para acceder a la camara", Toast.LENGTH_LONG).show();
                 }
-
+            }
+            case REQUEST_CODE:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
             }
             break;
 
@@ -151,4 +194,46 @@ public class PublishPropertyActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fetchLocation();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fetchLocation();
+    }
+
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    assert supportMapFragment != null;
+                    supportMapFragment.getMapAsync(PublishPropertyActivity.this);
+                }
+            }
+        });
+    }
 }
