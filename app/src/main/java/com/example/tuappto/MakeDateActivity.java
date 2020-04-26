f184efaa7f70454b119d6ad6b529ae274d693ed5
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-04-26 01:47:29 -0500

    Se dejo como estaba

--- app/src/main/java/com/example/tuappto/MakeDateActivity.java
+++ app/src/main/java/com/example/tuappto/MakeDateActivity.java
@@ -1,32 +1,14 @@
 package com.example.tuappto;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.DialogFragment;
 
-import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.widget.TimePicker;
-
-import java.util.Calendar;
 
 public class MakeDateActivity extends AppCompatActivity {
-
-    TimePickerDialog timePickerDialog;
-    Calendar calendar = Calendar.getInstance();
-    ConstraintLayout constraintLayout = findViewById(R.id.prueba);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_date);
-        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-            }
-        },calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),android.text.format.DateFormat.is24HourFormat(this));
-
-        timePickerDialog.show();
     }
 }
