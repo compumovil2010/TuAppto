691704508d1d1b9e81f3229aac9286e0dc4455b7
Pablo Veintemilla <pbr.9816@gmail.com>
2020-05-23 12:38:00 -0500

    publishProperty ya publica la publicacion con la ubicacion seleccionada y no a propia

--- app/src/main/java/com/example/tuappto/FiltersActivity.java
+++ app/src/main/java/com/example/tuappto/FiltersActivity.java
@@ -105,15 +105,6 @@ public class FiltersActivity extends FragmentActivity implements OnMapReadyCallb
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         googleMap.addMarker(markerOptions);
-        if(currentLocation != null){
-            ofertas.add(new LatLng(currentLocation.getLatitude() +0.001, currentLocation.getLongitude() + 0.002));
-            ofertas.add(new LatLng(currentLocation.getLatitude() -0.001, currentLocation.getLongitude() + -0.003));
-            ofertas.add(new LatLng(currentLocation.getLatitude() + 0.003, currentLocation.getLongitude() + 0.002));
-            for(int i = 0; i < ofertas.size(); i++){
-                mMap.addMarker(new MarkerOptions().position(ofertas.get(i)).icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).title(""));
-            }
-        }
     }
     @Override
     protected void onResume() {
